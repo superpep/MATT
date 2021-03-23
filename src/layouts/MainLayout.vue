05de3febfc5a84311225ca90285e5054e280f215
Àlex <alexcrafteo1@outlook.es>
2021-03-23 19:51:04 +0100

    Minor first changes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -47,6 +47,7 @@
 </template>
 
 <script>
+import { Loading } from 'quasar'
 import EssentialLink from 'components/EssentialLink.vue'
 
 const linksData = [
@@ -95,6 +96,16 @@ const linksData = [
 ]
 
 export default {
+  preFetch ({ redirect }) {
+    Loading.show()
+    if (typeof (Storage) === 'undefined') { // Si no hay local/session storage mostraremos un mensaje de error ya que poco se podrá hacer. Lo hacemos en el layou para que así se ejecute en todas las páginas.
+      this.$q.notify({
+        type: 'negative',
+        message: 'No se ha encontrado Local/Session storage. La aplicación no funcionará de forma correcta.'
+      })
+    }
+    Loading.hide()
+  },
   name: 'MainLayout',
   components: { EssentialLink },
   data () {
