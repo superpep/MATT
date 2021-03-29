8fb374c117d8c725c89a3177a71e6fcdd776a287
Àlex <alexcrafteo1@outlook.es>
2021-03-29 23:12:41 +0200

    Página inicial una vez iniciada sesión

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="bg-secondary window-height window-width row justify-center items-center">
+    <StopWatch/>
+  </q-page>
+</template>
+
+<script>
+import StopWatch from 'components/StopWatch.vue'
+
+export default {
+  preFetch ({ store, redirect }) {
+    if (!store.state.gestinson.user.name) { // SI NO ESTEM LOGUEJATS
+      redirect({ path: '/' }) // REDIRECCIONEM A LOGIN
+    }
+  },
+  name: 'MainPage',
+  components: { StopWatch }
+}
+</script>
