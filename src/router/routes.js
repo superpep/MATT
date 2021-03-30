09fb07c65b105880dad2188754408de5840604ed
Àlex <alexcrafteo1@outlook.es>
2021-03-31 00:01:44 +0200

    Añadida nueva página de ajustes

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,8 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
-      { path: 'index', component: () => import('pages/MainPage.vue') }
+      { path: 'index', component: () => import('pages/MainPage.vue') },
+      { path: 'settings', component: () => import('pages/Settings.vue') }
     ]
   },
 
