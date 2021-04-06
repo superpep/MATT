f7ccd59f06ff9beec59621f7971ce9008685c89a
Àlex <alexcrafteo1@outlook.es>
2021-04-06 09:57:41 +0200

    Añadida ruta para gestión de usuarios

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,7 +6,8 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/Index.vue') },
       { path: 'index', component: () => import('pages/MainPage.vue') },
-      { path: 'settings', component: () => import('pages/Settings.vue') }
+      { path: 'settings', component: () => import('pages/Settings.vue') },
+      { path: 'user-management', component: () => import('pages/UserManagement.vue') }
     ]
   },
 
