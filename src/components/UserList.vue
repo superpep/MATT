4fdcc90cd38fe742be211eeb64969c1308812e0b
Àlex <alexcrafteo1@outlook.es>
2021-04-16 12:34:41 +0200

    La comprobación de eliminación se lleva a cabo dentro de vue.

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -24,25 +24,11 @@
                 </q-item>
               </q-list>
             </q-btn-dropdown>
-            <q-btn @click="selectDeleteUser(numUser)" flat round icon="delete" color="red" />
+            <q-btn @click="confirmDelete(numUser)" flat round icon="delete" color="red" />
           </div>
         </q-item-section>
       </q-item>
     </q-list>
-
-    <!-- CONFIRMACIÓ DE ELIMINACIÓN -->
-      <q-dialog v-if="selectedNumUser >= 0" v-model="confirmDelete" >
-        <q-card>
-          <q-card-section class="row items-center">
-            <span class="q-ml-sm">¿Estás seguro de querer eliminar al usuario {{ users[selectedNumUser].name }}?</span>
-          </q-card-section>
-          <q-card-actions align="right">
-            <q-btn  flat label="Cancelar" color="primary" @click="confirmDelete = false" v-close-popup />
-            <q-btn flat label="Eliminar" color="primary" @click="deleteSelectedUser()" v-close-popup />
-          </q-card-actions>
-        </q-card>
-      </q-dialog>
-    <!-- FIN CONFIRMACIÓ DE ELIMINACIÓN -->
   </div>
 </template>
 
@@ -50,13 +36,6 @@
 import { encrypt } from 'boot/encryption'
 export default {
   name: 'UserList',
-  data () {
-    return {
-      confirmDelete: false,
-      promptPasswd: false,
-      selectedNumUser: -1
-    }
-  },
   methods: {
     editName (numUser) {
       if (!this.$store.state.gestinson.user.isAdmin || this.$store.state.gestinson.user.dni !== this.users[numUser].dni) {
@@ -128,14 +107,17 @@ export default {
           })
       }
     },
-    selectDeleteUser (numUser) {
-      this.selectedNumUser = numUser
-      this.confirmDelete = true
-    },
-    deleteSelectedUser () {
-      this.$store.dispatch('gestinson/deleteUser', this.selectedNumUser)
-      this.selectedNumUser = -1
-      this.confirmDelete = false
+    confirmDelete (numUser) {
+      this.$q.dialog({
+        message: '¿Estás seguro de querer eliminar al usuario ' + this.users[numUser].name + '?',
+        ok: {
+          push: true,
+          label: 'Eliminar'
+        },
+        cancel: true
+      }).onOk(() => {
+        this.$store.dispatch('gestinson/deleteUser', numUser)
+      })
     }
   },
   computed: {
