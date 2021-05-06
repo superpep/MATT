9accd570c5374f2dcd05b205b508fad5e4617203
Àlex <alexcrafteo1@outlook.es>
2021-05-06 22:19:59 +0200

    Añadido selector de idioma

---
+++ src/components/LanguageSelector.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-select
+    v-model="lang"
+    :options="langOptions"
+    :label="$t('language')"
+    dense
+    borderless
+    emit-value
+    map-options
+    options-dense
+    style="min-width: 150px"
+  />
+</template>
+
+<script>
+export default {
+  name: 'LanguageSelector',
+  data () {
+    return {
+      lang: this.$i18n.locale,
+      langOptions: [
+        { value: 'en-us', label: this.$t('english') },
+        { value: 'es-es', label: this.$t('spanish') }
+      ]
+    }
+  },
+  watch: {
+    lang (lang) {
+      this.$i18n.locale = lang
+    }
+  }
+}
+</script>
