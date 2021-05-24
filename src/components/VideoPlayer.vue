75e65af0464f21be651b1a0a1373c1753932cede
Àlex <alexcrafteo1@outlook.es>
2021-05-24 11:49:16 +0200

    Cambios mínimos

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="q-pa-sm q-gutter-s row">
     <div class="text-center">
-      <video ref="videoRef" style="max-width: 100%; max-height: 50%" src="" id="video-container" playsinline webkit-playsinline controls/>
+      <video ref="videoRef" style="max-width: 100%; max-height: 50%" src="" id="video-container" playinline="true" controls/>
       <q-file filled v-model="video" class="bg-primary" :label="$t('click_to_select_video')" @input="videoReader" counter max-files="1">
         <template v-slot:append>
           <q-icon v-if="video !== null" name="close" :title="$t('remove_video')" @click="removeVideo" class="cursor-pointer"/>
